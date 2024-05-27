aba64da9e7d542efa2850f70ce12d3f2005c204c
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-05-26 21:28:48 -0500

    Finished unit 6 of Astro's blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://angad.me"
+  site: "https://angad.me",
+  integrations: [preact()]
 });
