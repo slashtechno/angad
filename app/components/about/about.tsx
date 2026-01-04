9ab1b33007a1720e4f5fe69c5ae921e14f0c561e
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-01-04 11:24:57 -0600

    added media query for about, use AI to create an example list

--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -3,7 +3,7 @@ import './about.css';
 export function About() {
   return (
     <div className="about-container">
-      <h1 className="about-title">Hi! I'm Angad</h1>
+      <h1 className="about-title">Hi! I'm <span id="name">Angad!</span></h1>
       <p className="about-description">
         I'm a high-school student, photographer, and software developer. Currently, I'm contracting @ <a href="https://hackclub.com/">Hack Club</a> doing software development. 
       </p>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
-import { About } from "../about/about";
+import { About } from "../components/about/about";
+import "./home.css";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,21 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <About />;
+  return (
+    <>
+      {/* 
+        Centered nav list using flexbox
+        - flex-direction: column stacks links vertically
+        - justify-content: center centers them on the vertical axis
+        - align-items: center centers them on the horizontal axis
+        - height: 100vh makes it take up full viewport height
+      */}
+      <About />
+      <nav className="centered-nav">
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </>
+  );
 }
