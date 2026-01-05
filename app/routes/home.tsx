5d02cdfdb2ac87329e1d6dcc77aa6c6822a346f6
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-01-04 19:13:59 -0600

    control spacing between about and navlist Be explicit about setting Inter as the font

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,13 +5,16 @@ import "./home.css";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Angad Behl - Home" },
-    { name: "description", content: "Student, software developer, and photographer." },
+    {
+      name: "description",
+      content: "Student, software developer, and photographer.",
+    },
   ];
 }
 
 export default function Home() {
   return (
-<div id="homepage">
+    <div id="homepage">
       {/* 
         Centered nav list using flexbox
         - flex-direction: column stacks links vertically
@@ -19,7 +22,9 @@ export default function Home() {
         - align-items: center centers them on the horizontal axis
         - ~~height: 100vh makes it take up full viewport height~~ Right below the about section so we can't do 100vh, since otherwise, it would push the about section offscreen (not necessarily the content, but at least the box)
       */}
-      <About />
+      <div id="about">
+        <About />
+      </div>
       <nav className="centered-nav">
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
