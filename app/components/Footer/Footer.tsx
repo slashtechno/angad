85361362b2371362fb1b0605a6f2e4fdc37f82bb
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-01-06 20:23:39 -0600

    only show footer when not on root

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import "./Footer.css"
 export default function Footer() {
+    // https://reactrouter.com/api/hooks/useLocation
+    let location = useLocation();
+
     return (
         <footer>
         <p>© 2026 Angad Behl. All rights reserved.</p>
+        {/* https://react.dev/learn/conditional-rendering#logical-and-operator- */}
+        {location.pathname !== "/" && (
         <div className="links">
+            {/* With the magic of Inter's contextual alternates, this becomes an arrow */}
             <Link to="/">{"<-"} Back home</Link>
         </div>
+        )}
         </footer>
     );
     }
