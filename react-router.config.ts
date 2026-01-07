4a27e81caafaf67e1fbb0c9c8032f9558f8bb0f0
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-01-06 21:48:36 -0600

    feat: allow it to be built static

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,27 @@
 import type { Config } from "@react-router/dev/config";
+import { loadAllPostsParsed } from "./app/.server/posts";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
 
   // https://reactrouter.com/start/framework/data-loading#static-data-loading
+  async prerender() {
+    // Load all posts to generate routes for each musing
+    const posts = await loadAllPostsParsed();
+    
+    // Generate prerender routes for all posts
+    const musingRoutes = posts.map((post) => post.relativeHref);
+    
+    // Return all routes to prerender: home, musings list, and all individual musings
+    return [
+      "/",
+      "/musings",
+      ...musingRoutes,
+    ];
+  },
 
-  ssr: true,
+  ssr: false,
+
+
 } satisfies Config;
