5e20dbf9ef89ef5cdb94b46810743bffcc3b2141
slashtechno <77907286+slashtechno@users.noreply.github.com>
2026-01-10 16:44:26 -0600

    fix: remove redundant text in About component description

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -5,7 +5,7 @@ export function About() {
     <div className="about-container">
       <h1 className="about-title">Hi! I'm <span id="name">Angad!</span></h1>
       <p className="about-description">
-        I'm a high-school student, photographer, and software developer. Currently, I'm contracting @ <a href="https://hackclub.com/">Hack Club</a> doing software development. 
+        I'm a high-school student, photographer, and software developer. Currently, I'm contracting @ <a href="https://hackclub.com/">Hack Club</a>.
       </p>
     </div>
   );
